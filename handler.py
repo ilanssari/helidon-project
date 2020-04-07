9922c23c112b98cc64ea993ea740980cefb038e5
Issam Lanssari <issam.lanssari@oracle.com>
2020-04-07 16:38:07 +0100

    ** test 3

--- handler.py
+++ handler.py
@@ -46,7 +46,7 @@ def patche():
     origbranch = os.popen('echo ${CI_COMMIT_REF_NAME}').read()
     token = os.popen('echo ${CI_TOKEN}').read()
     remoteurl = os.popen("git config --get remote.origin.url | sed -E  's/(.*)@(.*)/\2/'").read()
-    print(remoteurl)
+    print("start#" + remoteurl + "#end")
     #os.system("git remote set-url origin https://gitlab-ci-token:" + token + remoteurl)
     #os.system("git checkout -b patch-${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHA}")
     #os.system('git add pom.xml && git commit -m "patche vulnerabilities for ${CI_COMMIT_REF_NAME}"')
