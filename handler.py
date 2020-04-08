10b4706cc2d69d07e8ef2abf4192930e22846ef2
Administrator <admin@example.com>
2020-04-08 10:48:54 +0000

    ** test 001

--- handler.py
+++ handler.py
@@ -36,8 +36,49 @@ def vulcheck():
                 #print(text["fixedVersion"].split(':')[1])
                 version.text = text["fixedVersion"].split(':')[1]
                 isvul = True
-    #os.system("rm -f pomtest.xml")
-    #tree.write("pomtest.xml")  
+    os.system("rm -f pom.xml")
+    tree.write("pom.xml")
     exit(isvul)
+
+def patche():
+    import os
+    
+    origbranch = os.popen('echo ${CI_COMMIT_REF_NAME}').read().strip()
+    token = os.popen('echo ${CI_TOKEN}').read().strip()
+    remoteurl = os.popen('echo ${REMOTE_URL}').read().strip()
+    #os.system("cat pom.xmlz 2> /dev/null 1> /dev/null")
+    #print(int(os.popen('cat pom.xmlz 2> /dev/null 1> /dev/null; echo $?').read().strip()))
+    #print(os.popen("echo 'https://gitlab-ci-token:" + token + "@" + remoteurl + "'").read())
+    os.system("git remote set-url origin https://gitlab-ci-token:" + token + "@" + remoteurl)
+    os.system("git checkout -b patch-${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHA}")
+    os.system('git add pom.xml && git commit -m "patche vulnerabilities for ${CI_COMMIT_REF_NAME}"')
+    code, out, err = runcommand('git push origin patch-${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHA} -o merge_request.create -o merge_request.target=' + origbranch + ' -o merge_request.remove_source_branch -o merge_request.title="patche vulnerabilities" -o merge_request.description="patche vulnerabilities"')
+    if code != 0:
+        print(err)
+        exit(True)
+    else:
+        print(out)
+        exit(False)
+    os.system('bash')
+    cmd = "catdsfs"
+    #returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
+    #print('returned value:', returned_value)
+    #os.environ["koka"] = 'mama'
+    #print(os.environ.get('koka'))
+    #code, out, err = runcommand(cmd)
+    #print("Return code: {}".format(code))
+    #print("stdout:" + out)
+    #print("stderr:" + err)
+
+def runcommand (cmd):
+    import os, subprocess
+    proc = subprocess.Popen(cmd,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            shell=True,
+                            universal_newlines=True)
+    std_out, std_err = proc.communicate()
+    return proc.returncode, std_out, std_err
+
 
 
